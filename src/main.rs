6f1f1b522efc2868c4f4e7b8a24dcd4c9ff9eb76
Anas Zughayyar <anaszughayyar@neobook.local>
2024-10-28 13:15:03 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 
 // Guessing Game:
 
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("The secret number is: {}", secret_number);
 
-    println!("you guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
 
+        let mut guess = String::new();
 
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;       
+            }
+        }
+    }
+    
 }
